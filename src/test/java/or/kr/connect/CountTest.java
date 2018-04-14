44e7db6ee3b6c279a9653e80d7f29af76ddc9994
minuk8932 <minuk8932@naver.com>
2018-04-14 16:51:34 +0900

    add dependency and test project

---
+++ src/test/java/or/kr/connect/CountTest.java
@@ -0,0 +1,14 @@
+package or.kr.connect;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CountTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
