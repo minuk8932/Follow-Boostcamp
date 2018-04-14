3154176fc2f7fe58163927908a29be91c9bfde5f
minuk8932 <minuk8932@naver.com>
2018-04-14 17:07:53 +0900

    add comment

--- src/test/java/or/kr/connect/CountTest.java
+++ src/test/java/or/kr/connect/CountTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 public class CountTest {
 	private int count = 0;
-	@Before
-	public void setUp() {
+	@Before								// @BeforeClass, @AfterClass : 테스트 클래스마다 한 번 씩. static 메소드 앞에만 가능
+	public void setUp() {				// @Before 매번 테스트 전, @After 매번 테스트 후
 		System.out.println(count++);
 	}
 	
-	@Test
-	public void testPlus() {
+	@Test								// 실행 할 테스트 메소드
+	public void testPlus() {				// count 멤버 변수는 매 테스트 마다 새로 생성된다. (출력결과 참고)
 		System.out.println(count++);
 	}
 	
